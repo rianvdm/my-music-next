043dbe8bae0058c97a7766225b5a694c2531b750
Rian van der Merwe <rianvdm@gmail.com>
2024-08-26 10:08:22 -0700

    Favicon

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="images/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
