9da9b611b0aa1475c79d04e0e2c4c30b5bea10ff
Rian van der Merwe <rianvdm@gmail.com>
2024-08-26 09:22:07 -0700

    New site deploy

---
+++ app/artist/[artist]/page.js
@@ -0,0 +1,121 @@
+'use client';
+
+import Head from 'next/head';
+import Link from 'next/link'; 
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function ArtistPage({ params }) {
+    const { artist } = params;
+    const [artistDetails, setArtistDetails] = useState(null);
+    const [topAlbums, setTopAlbums] = useState([]);
+    const router = useRouter();
+
+useEffect(() => {
+    if (artist) {
+        async function fetchArtistData() {
+            try {
+                // Decode the artist name before making the request
+                const decodedArtist = decodeURIComponent(artist);
+
+                const artistResponse = await fetch(`https://api-lastfm-artistdetail.rian-db8.workers.dev?artist=${encodeURIComponent(decodedArtist)}`);
+                let artistData = await artistResponse.json();
+                console.log('Fetched Artist Data:', artistData);
+
+                // Remove the Creative Commons text from the bio
+                if (artistData.bio) {
+                    artistData.bio = artistData.bio.replace(/User-contributed text is available under the Creative Commons By-SA License; additional terms may apply\./g, '').trim();
+                }
+
+                setArtistDetails(artistData);
+
+                // Fetch top albums for the artist
+                const albumsResponse = await fetch(`https://api-lastfm-artisttopalbums.rian-db8.workers.dev?artist=${encodeURIComponent(decodedArtist)}`);
+                const albumsData = await albumsResponse.json();
+                console.log('Fetched Albums Data:', albumsData);
+
+                setTopAlbums(albumsData.topAlbums.slice(0, 3)); // Limit to top 3 albums
+            } catch (error) {
+                console.error('Error fetching artist data:', error);
+            }
+        }
+        fetchArtistData();
+    }
+}, [artist]);
+
+    if (!artistDetails) {
+        return <p>Loading...</p>;
+    }
+
+    // Function to convert newline characters to paragraphs and line breaks
+    const renderBioContent = (content) => {
+        return content.split('\n\n').map((paragraph, index) => (
+            <p key={index} dangerouslySetInnerHTML={{ __html: paragraph.replace(/\n/g, '<br />') }} />
+        ));
+    };
+
+    return (
+        <div>
+            <Head>
+                <title>{artistDetails.name} - Artist Details</title>
+                <meta name="description" content={`Details about the artist ${artistDetails.name}`} />
+            </Head>
+            <div className="track_ul2" style={{ paddingTop: '20px' }}>
+            <Link href="#" onClick={(e) => { 
+                e.preventDefault(); 
+                router.back(); 
+            }} style={{ color: 'var(--c-accent)', textDecoration: 'none', marginRight: '10px' }}>
+                ← Back
+            </Link>
+                <Link href="/" style={{ color: 'var(--c-accent)', textDecoration: 'none' }}>
+                    Home
+                </Link>
+            </div>
+            <header>
+                <h1 style={{ marginTop: '0.4em' }}>{artistDetails.name}</h1>
+            </header>
+            <main>
+                <div className="track_ul2">
+                    <div className="image-text-wrapper">
+                        <img 
+                            src={artistDetails.image} 
+                            alt={artistDetails.name} 
+                            style={{ maxWidth: '100%', width: '220px', height: 'auto' }} 
+                        />
+                        <div className="no-wrap-text">
+                            <p><strong>My playcount:</strong> {artistDetails.userplaycount} tracks</p>
+                            <p><strong>Genres:</strong> {artistDetails.tags.join(', ')}</p>
+
+
+                            <p style={{ marginBottom: '0.2em' }}><strong>Top 3 Albums:</strong></p>
+                            <ul style={{ listStyleType: 'none', paddingLeft: '0', marginTop: '0' }}>
+                                {topAlbums.map((album, index) => (
+                                    <li key={index}>
+                                        <a href={album.url} target="_blank" rel="noopener noreferrer">
+                                            {album.name}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <p style={{ marginBottom: '0.2em' }}><strong>Similar Artists:</strong></p>
+                    <ul style={{ listStyleType: 'none', paddingLeft: '0', marginTop: '0' }}>
+                        {artistDetails.similar.map((artist, index) => (
+                            <li key={index}>
+                                <Link href={`/artist/${encodeURIComponent(artist)}`} rel="noopener noreferrer">
+                                    {artist}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                <strong>Artist bio:</strong>
+                </div>
+                {renderBioContent(artistDetails.bio)}
+            </main>
+            <footer className="footer">
+                <p><a href="https://youtu.be/cNtprycno14?t=9036">There’s a fire that’s been burning right outside my door.</a></p>
+            </footer>
+        </div>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,171 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import Head from 'next/head';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          GHelloi?&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
+    const [recentTracksData, setRecentTracksData] = useState(null);
+    const [topArtistsData, setTopArtistsData] = useState(null);
+    const [topAlbumsData, setTopAlbumsData] = useState(null);
+    const [dayGreeting, setDayGreeting] = useState('');
+
+    useEffect(() => {
+        // Set the greeting based on the current day in the user's time zone
+        const setGreeting = () => {
+            const options = { weekday: 'long' };
+            const today = new Date();
+            const dayName = new Intl.DateTimeFormat('en-US', options).format(today);
+            setDayGreeting(`Happy ${dayName}, friend!`);
+        };
+
+        setGreeting();
+
+        // Fetch recent tracks separately to ensure it can render as soon as it's ready
+        const fetchRecentTracks = async () => {
+            try {
+                console.time('fetchRecentTracks');
+                const recentTracksResponse = await fetch('https://api-lastfm-recenttracks.rian-db8.workers.dev');
+                const recentTracksData = await recentTracksResponse.json();
+                console.timeEnd('fetchRecentTracks');
+                setRecentTracksData(recentTracksData);
+            } catch (error) {
+                console.error('Error fetching recent tracks data:', error);
+            }
+        };
+
+        const fetchTopArtists = async () => {
+            try {
+                const topArtistsResponse = await fetch('https://api-lastfm-topartists.rian-db8.workers.dev');
+                const topArtistsData = await topArtistsResponse.json();
+
+                // Fetch artist details for each artist to get the image and other details
+                const detailedArtistsData = await Promise.all(topArtistsData.map(async (artist) => {
+                    const detailResponse = await fetch(`https://api-lastfm-artistdetail.rian-db8.workers.dev?artist=${encodeURIComponent(artist.name)}`);
+                    const detailData = await detailResponse.json();
+                    return {
+                        name: detailData.name,
+                        playcount: artist.playcount,
+                        url: detailData.url,
+                        image: detailData.image,
+                        bio: detailData.bio,
+                    };
+                }));
+
+                setTopArtistsData(detailedArtistsData);
+            } catch (error) {
+                console.error('Error fetching top artists data:', error);
+            }
+        };
+
+        const fetchTopAlbums = async () => {
+            try {
+                const topAlbumsResponse = await fetch('https://api-lastfm-topalbums.rian-db8.workers.dev');
+                const topAlbumsData = await topAlbumsResponse.json();
+                setTopAlbumsData(topAlbumsData.slice(0, 6)); // Limit to top 6 albums
+            } catch (error) {
+                console.error('Error fetching top albums:', error);
+            }
+        };
+
+        fetchRecentTracks();
+        fetchTopArtists();
+        fetchTopAlbums();
+    }, []);
+
+    const renderTopArtists = () => {
+        if (!topArtistsData) {
+            return <p>Loading artists...</p>;
+        }
+
+        return (
+            <div className="track-grid">
+                {topArtistsData.slice(0, 6).map(artist => (
+                    <div className="track" key={artist.name}>
+                        <Link href={`artist/${encodeURIComponent(artist.name)}`} rel="noopener noreferrer">
+                            <img src={artist.image || '/path/to/default/image.png'} className="track_image" alt={artist.name} />
+                        </Link>
+                        <div className="track_content">
+                            <h2 className="track_artist">
+                                <Link href={`artist/${encodeURIComponent(artist.name)}`} rel="noopener noreferrer">
+                                    {artist.name}
+                                </Link>
+                            </h2>
+                            <p className="track_name">{artist.playcount} plays</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const renderTopAlbums = () => {
+        if (!topAlbumsData) {
+            return <p>Loading albums...</p>;
+        }
+
+        return (
+            <div className="track-grid">
+                {topAlbumsData.map(album => (
+                    <div className="track" key={album.name}>
+                        <a href={album.albumUrl} target="_blank" rel="noopener noreferrer">
+                            <img src={album.image} className="track_image" alt={album.name} />
+                        </a>
+                        <div className="track_content">
+                            <h2 className="track_artist">{album.name}</h2>
+                            <p className="track_name">{album.artist}</p>
+                            <p className="track_album">{album.playcount} songs played</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+    const renderRecentTracks = () => {
+        if (!recentTracksData) {
+            return <p>Loading recent stats...</p>;
+        }
+
+        return (
+            <p>
+                Over the last 7 days I listened to <strong>{new Intl.NumberFormat().format(recentTracksData.playcount)} tracks</strong> across <strong>{new Intl.NumberFormat().format(recentTracksData.artist_count)} artists</strong> and <strong>{new Intl.NumberFormat().format(recentTracksData.album_count)} albums</strong>. The last artist I listened to was <Link href={`artist/${encodeURIComponent(recentTracksData.last_artist)}`} rel="noopener noreferrer"><strong>{recentTracksData.last_artist}</strong></Link>.
+            </p>
+        );
+    };
+
+    return (
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <Head>
+                <title>Rian&rsquo;s Music</title>
+                <meta name="description" content="Tracking my Last.fm music listening habits." />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <header>
+                <h1>{dayGreeting}</h1>
+            </header>
+            <main>
+                <section id="lastfm-stats">
+                    {renderRecentTracks()}
+                    <p style={{ textAlign: 'center' }}>
+                      🚧 <em>I am slowly porting <a href="https://music.elezea.com">the old music site</a> over to Cloudflare <a href="https://pages.cloudflare.com/">Pages</a> and <a href="https://workers.cloudflare.com/">Workers</a>. Things might be a bit messy for a while.</em> 🚧
+                    </p>
+                    <h2>👩‍🎤 Top Artists</h2>
+                    <p style={{ textAlign: 'center' }}>
+                        <strong>The top artists I listened to in the past 7 days.</strong>
+                    </p>
+                    {renderTopArtists()}  {/* Render top artists */}
+                    <h2>🏆 Top Albums</h2>
+                    <p style={{ textAlign: 'center' }}>
+                        <strong>The top albums I listened to in the past 7 days.</strong>
+                    </p>
+                    {renderTopAlbums()}  {/* Render top albums */}
+                </section>
+            </main>
+            <div className="footer">
+                <p><a href="https://youtu.be/cNtprycno14?t=9036">There’s a fire that’s been burning right outside my door.</a></p>
+            </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    );
 }
