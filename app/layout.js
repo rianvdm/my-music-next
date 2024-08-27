27603e0e37667ee71dfae0646dc6674ee559f4dc
Rian van der Merwe <rianvdm@gmail.com>
2024-08-27 10:44:26 -0700

    Clean up metadata

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,6 @@ export default function RootLayout({ children }) {
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Rian van der Merwe" />
-        <link href="/styles.css?v=16" rel="stylesheet" />
         <link rel="shortcut icon" type="image/png" href="/images/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
@@ -28,7 +27,7 @@ export default function RootLayout({ children }) {
         <meta name="description" content="Real-time listening data and music recommendations." />
 
         {/* Open Graph / Facebook Meta Tags */}
-        <meta property="og:url" content="https://music.rianvdm.com/about" />
+        <meta property="og:url" content="https://music.rianvdm.com/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Rian's music" />
         <meta property="og:description" content="Real-time listening data and music recommendations." />
@@ -37,7 +36,7 @@ export default function RootLayout({ children }) {
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="music.rianvdm.com" />
-        <meta property="twitter:url" content="https://music.rianvdm.com/about" />
+        <meta property="twitter:url" content="https://music.rianvdm.com/" />
         <meta name="twitter:title" content="Rian's music" />
         <meta name="twitter:description" content="Real-time listening data and music recommendations." />
         <meta name="twitter:image" content="https://files.elezea.com/alberto-bigoni-4-DeS5a-hAM-unsplash.jpg" />
