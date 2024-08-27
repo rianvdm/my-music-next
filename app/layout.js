290fc9500c7e3cdce80eb87de2829161899ec8b3
Rian van der Merwe <rianvdm@gmail.com>
2024-08-27 09:38:24 -0700

    Add metadata

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,33 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="images/favicon.ico" />
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Rian van der Merwe" />
+        <link href="/styles.css?v=16" rel="stylesheet" />
+        <link rel="shortcut icon" type="image/png" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
+
+        {/* HTML Meta Tags */}
+        <meta name="description" content="Real-time listening data and music recommendations." />
+
+        {/* Open Graph / Facebook Meta Tags */}
+        <meta property="og:url" content="https://music.rianvdm.com/about" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="About Rian's music" />
+        <meta property="og:description" content="Real-time listening data and music recommendations." />
+        <meta property="og:image" content="https://files.elezea.com/alberto-bigoni-4-DeS5a-hAM-unsplash.jpg" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="music.rianvdm.com" />
+        <meta property="twitter:url" content="https://music.rianvdm.com/about" />
+        <meta name="twitter:title" content="About Rian's music" />
+        <meta name="twitter:description" content="Real-time listening data and music recommendations for your enjoyment." />
+        <meta name="twitter:image" content="https://files.elezea.com/alberto-bigoni-4-DeS5a-hAM-unsplash.jpg" />
       </head>
       <body className={inter.className}>
         <NavBar />
