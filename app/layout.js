a95a78a2fdeffc006151568874467dcc3847ea23
Rian van der Merwe <rianvdm@gmail.com>
2024-08-28 19:26:45 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Rian van der Merwe" />
         <link rel="shortcut icon" type="image/png" href="/images/favicon.ico" />
