fe12b55380fb55fbac0eef50d2d04e3537abd90c
Rian van der Merwe <rianvdm@gmail.com>
2024-08-31 07:03:27 -0700

    Correct KV URL

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 My name is [Rian van der Merwe](https://elezea.com/) and this is a side project about the music I listen to. It pulls data from [my Last.fm activity](https://www.last.fm/user/bordesak) and [Discogs collection](https://www.discogs.com/user/elezea-records/collection), and then I ✨enhance✨ the data using the APIs for ChatGPT and Spotify. Like most side projects, it's always a work in progress.
 
-The site is built on [Cloudflare](https://cloudflare.com/) products, including [Pages](https://pages.cloudflare.com/), [Workers](https://workers.cloudflare.com/), and [Workers KV](https://developers.cloudflare.com/kv/). It's a wonderful set of tools to work with—and I'm not just saying that because [I work there](https://elezea.com/portfolio/).
+The site is built on [Cloudflare](https://cloudflare.com/) products, including [Pages](https://pages.cloudflare.com/), [Workers](https://workers.cloudflare.com/), and [Workers KV](https://www.cloudflare.com/developer-platform/workers-kv/). It's a wonderful set of tools to work with—and I'm not just saying that because [I work there](https://elezea.com/portfolio/).
 
 If you want to chat about this project, feel free to [reach out](https://elezea.com/contact/)!
--- app/about/page.js
+++ app/about/page.js
@@ -4,7 +4,7 @@ export default function AboutPage() {
       <h1>About Rian’s Music</h1>
         <h3>Hello 👋</h3>
           <p>My name is <a href="https://elezea.com">Rian van der Merwe</a> and this is a side project about the music I listen to. It pulls data from <a href="https://www.last.fm/user/bordesak">my Last.fm activity</a> and <a href="https://www.discogs.com/user/elezea-records/collection">Discogs collection</a>,  and then I ✨enhance✨ the data using the APIs for ChatGPT and Spotify. Like most side projects, it's always a work in progress.</p>
-          <p>The site is built on <a href="https://cloudflare.com">Cloudflare</a> products, including <a href="https://pages.cloudflare.com/">Pages</a>, <a href="https://workers.cloudflare.com/">Workers</a>, and <a href="https://developers.cloudflare.com/kv/">Workers KV</a>.  It's a wonderful set of tools to work with—and I'm not just saying that because <a href="https://elezea.com/portfolio/">I work there</a>.</p>
+          <p>The site is built on <a href="https://cloudflare.com">Cloudflare</a> products, including <a href="https://pages.cloudflare.com/">Pages</a>, <a href="https://workers.cloudflare.com/">Workers</a>, and <a href="https://www.cloudflare.com/developer-platform/workers-kv/">Workers KV</a>.  It's a wonderful set of tools to work with—and I'm not just saying that because <a href="https://elezea.com/portfolio/">I work there</a>.</p>
           <p>If you want to chat about this project, feel free to <a href="https://elezea.com/contact/">reach out</a>!</p>
     </div>
   );
