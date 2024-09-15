935e20a11b384cfe23f4de14dc7bca813b3f1d6e
Rian van der Merwe <rianvdm@gmail.com>
2024-09-15 07:15:51 -0700

    Update index.js

--- workers/api-lastfm-topalbums/src/index.js
+++ workers/api-lastfm-topalbums/src/index.js
@@ -6,13 +6,15 @@ export default {
         const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${username}&api_key=${apiKey}&period=7day&limit=6&format=json`);
         const data = await response.json();
 
+        const backupImageUrl = 'https://file.elezea.com/noun-no-image.png';
+
         const topAlbums = data.topalbums?.album?.map(album => ({
             artist: album.artist.name,
             artistUrl: album.artist.url,
             name: album.name,
             playcount: parseInt(album.playcount, 10),
             albumUrl: album.url,
-            image: album.image?.find(img => img.size === 'extralarge')?.['#text'] || ''
+            image: album.image?.find(img => img.size === 'extralarge')?.['#text'] || backupImageUrl // Use backup image if none found
         })) || [];
 
         return new Response(JSON.stringify(topAlbums), {
