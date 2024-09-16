593c2b075a928fb4950a41cd1c2f2d4e53746e2f
Rian van der Merwe <rianvdm@gmail.com>
2024-09-16 13:55:44 -0700

    Text edits

--- app/about/page.js
+++ app/about/page.js
@@ -15,7 +15,11 @@ export default function AboutPage() {
                 const response = await fetch('https://raw.githubusercontent.com/rianvdm/my-music-next/main/README.md');
                 const markdown = await response.text();
                 const htmlContent = marked(markdown);
-                setAboutContent(htmlContent);
+
+                // Modify the HTML content to add the 'centered-image' class to all <img> tags
+                const modifiedContent = htmlContent.replace(/<img/g, '<img class="centered-image"');
+                
+                setAboutContent(modifiedContent);
             } catch (error) {
                 console.error('Error fetching about content:', error);
                 setAboutContent('<p>Failed to load about content.</p>');
@@ -29,7 +33,7 @@ export default function AboutPage() {
         <div>
             <h1>About this site</h1>
             {/* Render the fetched and parsed README.md content */}
-            <div className="about-content" dangerouslySetInnerHTML={{ __html: aboutContent }} />
+            <div dangerouslySetInnerHTML={{ __html: aboutContent }} />
         </div>
     );
 }
