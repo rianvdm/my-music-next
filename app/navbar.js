8da6317bd3d8ccadd5dc0666559dc5545b272334
Rian van der Merwe <rianvdm@gmail.com>
2024-09-28 14:30:05 -0700

    Update navbar.js

--- app/navbar.js
+++ app/navbar.js
@@ -46,7 +46,7 @@ export default function NavBar() {
           <Link href="/recommendations">Get rec’d</Link>
         </li>
         <li style={liStyle}>
-          <Link href="/about">About</Link>
+          <Link href="/about">About/Discord</Link>
         </li>
       </ul>
 
