6aa2bb0ed2e7463aa9aa7cb19709f55c90793ab6
Rian van der Merwe <rianvdm@gmail.com>
2024-09-30 05:53:29 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -220,9 +220,9 @@ export default function Home() {
                     <h2 style={{ marginBottom: 0, marginTop: "2em" }}>💿 Learn more about an album</h2>
                     <AlbumSearch /> {/* Album search functionality goes here */}
 
-                    <h2>👂 Give these recent searches a try</h2>
+                    <h2>👀 Recent searches for inspiration</h2>
                     <p style={{ textAlign: 'center' }}>
-                        <strong>Here are some albums that people recently searched for (and probably told their friends about):</strong>
+                        <strong>Here are some albums that the community recently searched for.</strong>
                     </p><br/>
                     <Suspense fallback={<p>Loading recent searches...</p>}>
                         <RecentSearches data={recentSearchesData} />
