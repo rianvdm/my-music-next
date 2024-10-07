46944653c566362dc5a9e233d05f0ec0c89f7f37
Rian van der Merwe <rianvdm@gmail.com>
2024-10-07 10:07:26 -0700

    Package updates

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from './navbar';
 import { headers } from 'next/headers';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
