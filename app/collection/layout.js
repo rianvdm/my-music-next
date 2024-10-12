189ef392a60b9e261f5136ff2e0134181f432ab3
Rian van der Merwe <rianvdm@gmail.com>
2024-10-12 09:19:42 -0700

    Metadata

---
+++ app/collection/layout.js
@@ -0,0 +1,22 @@
+export async function generateMetadata() {
+  
+    return {
+      title: `Stats about my collection`,
+      description: `All about that physical media.`,
+      openGraph: {
+        title: `Stats about my collection`,
+        description: `All about that physical media.`,
+        images: `https://file.elezea.com/my_collection.png`,
+      },
+      twitter: {
+        card: 'summary',
+        title: `Stats about my collection`,
+        description: `All about that physical media.`,
+        images: `https://file.elezea.com/my_collection.png`,
+      },
+    }
+  }
+  
+  export default function Layout({ children }) {
+    return children
+  }
