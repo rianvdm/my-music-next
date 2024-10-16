c69864b8b10c14b8b189226b2b6c917d21c1d59c
Rian van der Merwe <rianvdm@gmail.com>
2024-10-16 12:48:55 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+// next.config.mjs
+
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-// Use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
+/**
+ * Use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
+ */
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
 }
@@ -8,7 +12,22 @@ if (process.env.NODE_ENV === 'development') {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['i.discogs.com', 'file.elezea.com'], // Allow these external domains
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.discogs.com',
+        pathname: '/**', // Allows all paths on this hostname
+      },
+      {
+        protocol: 'https',
+        hostname: 'file.elezea.com',
+        pathname: '/**', // Allows all paths on this hostname
+      },
+    ],
+    // Optional: Additional image configuration settings
+    // deviceSizes: [320, 420, 768, 1024, 1200],
+    // imageSizes: [16, 32, 48, 64, 96],
+    // minimumCacheTTL: 60,
   },
 };
 
