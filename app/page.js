dc78f7969d677b95d87bc460dea0a6e6f54ad2f9
Rian van der Merwe <rianvdm@gmail.com>
2024-11-17 08:03:24 -0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -178,7 +178,7 @@ export default function Home() {
             <strong>
               <Link href="/recommendations">get rec'd</Link>
             </strong>
-            . Or maybe explore a random genre like{' '}
+            . Or maybe explore the history and seminal albums of a random genre like{' '}
             <strong>
               <Link href={`/genre/${urlGenre}`}>{displayGenre}</Link>
             </strong>
