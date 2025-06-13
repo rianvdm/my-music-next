8513000f81abdf4d5a789e1988cabe7905473eb1
Rian van der Merwe <rianvdm@gmail.com>
2025-06-13 09:24:25 -0700

    Test pre-commit hooks

---
+++ test-formatting.js
@@ -0,0 +1,10 @@
+// Test file with bad formatting
+const badly_formatted = {
+  key: 'value',
+  another: 'test',
+};
+
+function testFunction() {
+  console.log('no indentation');
+  return badly_formatted;
+}
